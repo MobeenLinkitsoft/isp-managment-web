e43b44285904bdd75d3fbfdba3b5e59ab07fbb3e
Malik Mubeen <141817000+MobeenLinkitsoft@users.noreply.github.com>
2025-09-15 01:44:34 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,30 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)", // apply everywhere
+        source: "/(.*)",
         headers: [
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' https://js.stripe.com; style-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none'; base-uri 'self';",
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-inline' https://js.stripe.com; " +
+              "connect-src 'self' http://52.3.153.225:1337 https://api.stripe.com wss:; " +
+              "style-src 'self' 'unsafe-inline'; " +
+              "img-src 'self' data:; " +
+              "object-src 'none'; " +
+              "base-uri 'self'; " +
+              "frame-ancestors 'none';",
           },
         ],
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://52.3.153.225:1337/:path*", // your backend
       },
     ];
   },
